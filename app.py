ca827b3f283f2a2d98c79935098e78e1fc36a168
Srijam Sen <161827356+SRIJAM-SEN@users.noreply.github.com>
2025-12-11 02:51:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from pathlib import Path
 import pandas as pd
@@ -322,6 +323,10 @@ def meal_plan():
         message=message,
     )
 
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+##if __name__ == "__main__":
+  ##  app.run(debug=True)
+
